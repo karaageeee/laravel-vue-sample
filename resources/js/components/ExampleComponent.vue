ce3ebad5330ae28c3d97ddeb7943737993793b5d
YuseiTakao <yuseing335@@gmail.com>
2019-09-06 00:06:03 +0800

    added example

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,11 +1,25 @@
 <template>
     <div class="container">
-        Hello World
+        <div>Hello World</div>
+
+        <div><a :href="url" target="_blank">click me</a></div>
+
+        <div>
+            <button @click="flag = !flag">Change flag</button>
+            <div>{{ flag }}</div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        data: function () {
+            return {
+                message: 'hello world',
+                url: 'https://google.com',
+                flag: true,
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
