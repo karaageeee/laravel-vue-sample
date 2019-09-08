5918daa897ac4a1140467a0360f3f45b72b6f3d6
YuseiTakao <yuseing335@@gmail.com>
2019-09-08 16:08:58 +0800

    added computed and methods

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,13 +1,33 @@
 <template>
     <div class="container">
-        <div>Hello World</div>
 
-        <div><a :href="url" target="_blank">click me</a></div>
+        <!-- hello world -->
+        <div>{{ message }}</div>
 
-        <div>
-            <button @click="flag = !flag">Change flag</button>
+        <!-- v-bind -->
+        <div style="margin-top: 20px; border-top: 1px solid;">
+            <a :href="url" target="_blank">click me to go to Google</a>
+        </div>
+
+        <!-- v-on -->
+        <div style="margin-top: 20px; border-top: 1px solid;">
+            <button @click="flag = !flag">Toggle flag blow</button>
             <div>{{ flag }}</div>
         </div>
+
+        <!-- computed -->
+        <div style="margin-top: 20px; border-top: 1px solid;">
+            computed message
+            <div>{{ computedShowMessage }}</div>
+            <button @click="message += ' clicked'">Change message</button>
+        </div>
+
+        <!-- metohd -->
+        <div style="margin-top: 20px; border-top: 1px solid;">
+            metohd message
+            <div v-text="showMessage()"></div>
+        </div>
+
     </div>
 </template>
 
@@ -22,6 +42,18 @@
         },
         mounted() {
             console.log('Component mounted.')
-        }
+        },
+        computed: {
+            computedShowMessage: function () {
+                console.log('executed computed showMessage')
+                return this.message
+            }
+        },
+        methods: {
+            showMessage: function () {
+                console.log('executed method showMessage')
+                return this.message
+            }
+        },
     }
 </script>
